6666a54ae69d012885acfc7ad08b7049599f5d54
hung.nguyen <hung.nguyen>
2025-12-26 13:40:23 +0700

    feat(task-041): implement app logo and splash screen, finalize sprint 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
 import { ThemeProvider } from '@theme';
+import RNBootSplash from 'react-native-bootsplash';
 import './i18n';
 
 const styles = StyleSheet.create({
@@ -12,14 +13,27 @@ const styles = StyleSheet.create({
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { RootNavigator } from './navigation';
 
-const App = (): JSX.Element => (
-    <GestureHandlerRootView style={styles.root}>
-        <ThemeProvider>
-            <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
-            <RootNavigator />
-        </ThemeProvider>
-    </GestureHandlerRootView>
-);
+const App = (): JSX.Element => {
+    useEffect(() => {
+        const init = async () => {
+            // …do multiple sync or async tasks
+        };
+
+        init().finally(async () => {
+            await RNBootSplash.hide({ fade: true });
+            console.log('Bootsplash has been hidden successfully');
+        });
+    }, []);
+
+    return (
+        <GestureHandlerRootView style={styles.root}>
+            <ThemeProvider>
+                <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
+                <RootNavigator />
+            </ThemeProvider>
+        </GestureHandlerRootView>
+    );
+};
 
 
 export default App;

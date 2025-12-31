eaae58367f4117fd6a59f444d67b3fd60c884c08
hung.nguyen <hung.nguyen>
2025-12-31 12:17:00 +0700

    chore(firebase): ensure default app init

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
+import '@react-native-firebase/app';
 import App from './src/App';
 import { name as appName } from './app.json';
 import { notificationService } from './src/services/notifications/notificationService';
